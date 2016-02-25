c3028b172a29d8875d152627eb175314969e15be
mrbrett <brett@cirrously.com>
2016-02-24 17:18:26 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express               = require('express'),
+    app                   = express(),
+    redis                 = require('redis'),
+    url                   = require('url'),
+    emberDeploy           = require('node-ember-cli-deploy-redis'),
+    client                = require("redis").createClient(process.env.REDIS_PORT, process.env.REDIS_HOST),
+    EMBER_APP_NAME        = 'aventura';
+
+// Authorize our Redis Client -- could parse the password out of the redis url
+// but I made it a little bit more surefire (wasnt liking it) and made it a heroku env var
+client.auth(process.env.REDIS_PASS);
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.use('/*', emberDeploy('aventura:index', {
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASS}));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
