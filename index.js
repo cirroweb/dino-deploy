9a0496e6d8ff2fe5e07b6dbd8ea0d8d98f457226
mrbrett <brett@cirrously.com>
2016-03-23 15:18:19 -0600

    change aventura to dino

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express               = require('express'),
     url                   = require('url'),
     emberDeploy           = require('node-ember-cli-deploy-redis'),
     client                = require("redis").createClient(process.env.REDIS_PORT, process.env.REDIS_HOST),
-    EMBER_APP_NAME        = 'aventura';
+    EMBER_APP_NAME        = 'dinosaurJS';
 
 // Authorize our Redis Client -- could parse the password out of the redis url
 // but I made it a little bit more surefire (wasnt liking it) and made it a heroku env var
@@ -13,7 +13,7 @@ client.auth(process.env.REDIS_PASS);
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.use('/*', emberDeploy('aventura:index', {
+app.use('/*', emberDeploy('dinosaurJS:index', {
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
   password: process.env.REDIS_PASS}));
