a9dda1f4ebb62ac910cbe0904f482e4c7f80a44e
fbushell <fbushell@gmail.com>
2016-04-16 15:07:12 -0600

    added force ssl middleware

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express               = require('express'),
     url                   = require('url'),
     emberDeploy           = require('node-ember-cli-deploy-redis'),
     client                = require("redis").createClient(process.env.REDIS_PORT, process.env.REDIS_HOST),
+    forceSsl              = require('force-ssl-heroku');
     EMBER_APP_NAME        = 'dinosaurJS';
 
 // Authorize our Redis Client -- could parse the password out of the redis url
@@ -11,12 +12,16 @@ var express               = require('express'),
 client.auth(process.env.REDIS_PASS);
 
 app.set('port', (process.env.PORT || 5000));
+
+app.use(forceSsl);
 app.use(express.static(__dirname + '/public'));
 
 app.use('/*', emberDeploy('dinosaurJS:index', {
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
   password: process.env.REDIS_PASS}));
+
+app.get('/ping', pingHandler);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
